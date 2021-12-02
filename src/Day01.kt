9ba0ee7fd88e2c559b733533b1eac32d17573f9f
sjgoebel <sgoebel@gmail.com>
2021-12-01 22:45:59 -0600

    Day 1 in kotlin

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,38 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var result = 0
+        val numList = input.map { Integer.parseInt(it) }
+
+        var index = 0
+        while (index < numList.size - 1) {
+            if (numList[index + 1] > numList[index]) {
+                result++
+            }
+            index++
+        }
+        return result
     }
 
+
     fun part2(input: List<String>): Int {
-        return input.size
+        var result = 0
+        val numList = input.map { Integer.parseInt(it) }
+
+        var index = 0
+        while (index < numList.size - 4) {
+            if (numList[index] + numList[index + 1] + numList[index + 2] < numList[index + 1] + numList[index + 2] + numList[index + 3]) {
+                result++
+            }
+            index++
+        }
+
+
+        return result
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    //val testInput = readInput("Day01_test")
+    //check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
